0c630ff1776eae9997ade4744846b74a594d1954
Rishi Raj <rishiraj727909@gmail.com>
2022-04-22 22:51:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+require('express-async-errors');
+// express
+
+const express = require('express');
+const app = express();
+// rest of the packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
